69ebf7b46c52390136ab46b419d2bf48e9ec85d2
Marisol2201 <1529@holbertonschool.com>
2025-08-26 01:30:42 -0500

    Reto3

--- src/main/java/co/com/techskill/lab2/library/LibraryApplication.java
+++ src/main/java/co/com/techskill/lab2/library/LibraryApplication.java
@@ -1,15 +1,25 @@
 package co.com.techskill.lab2.library;
 
+import co.com.techskill.lab2.library.service.PetitionService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 @OpenAPIDefinition
 public class LibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
+
+        PetitionService service = new PetitionService();
+        Flux<String> flujo = service.actorOrquestator();
+
+        flujo.doOnNext(System.out::println)
+                .blockLast();
+
+        System.out.println("Fin");
+        //SpringApplication.run(LibraryApplication.class, args);
 	}
 
 }
