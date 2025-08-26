04be94745b9b2ee65832138994ee6edc91368ef3
Isabel Vallejo G <ivallejo@bancolombia.com.co>
2025-08-26 15:26:03 -0500

    se elimina metodo que no se uso

--- src/main/java/co/com/techskill/lab2/library/service/dummy/OrchestratorService.java
+++ src/main/java/co/com/techskill/lab2/library/service/dummy/OrchestratorService.java
@@ -36,6 +36,7 @@ public class OrchestratorService {
     }
 
     public Flux<String> dummyOrchestrate() {
+        //el dummyhighpriority solo traera peticiones de prioridad 7 o mas
         return petitionRepository.dummyHighPriority()
                 .limitRate(20)
                 .publishOn(Schedulers.boundedElastic())
@@ -44,6 +45,7 @@ public class OrchestratorService {
                         System.out.println(String.format("Petición encontrada con ID: %s de tipo %s",
                                 petition.getPetitionId(),petition.getType())))
                 //Fan-out
+                //aca se ordena las peticiones
                 .sort((p1, p2) -> Integer.compare(p2.getPriority(), p1.getPriority()))
                 .groupBy(petition -> petition.getType()) //LEND / RETURN
                 //Fan-in
@@ -86,37 +88,4 @@ public class OrchestratorService {
                 .doOnComplete(() -> System.out.println("Orchestration complete"));
     }
 
-    public Flux<String> dummyOrchestrateInspect() {
-        return petitionRepository.dummyFindAll()
-                .limitRate(20)
-                .publishOn(Schedulers.boundedElastic())
-                .doOnSubscribe(s -> System.out.println("Inicio orquestación..."))
-                .doOnNext(petition ->
-                        System.out.println(String.format("Petición encontrada con ID: %s de tipo %s",
-                                petition.getPetitionId(),petition.getType())))
-                //Fan-out
-                .groupBy(petition -> petition.getType()) //LEND / RETURN
-                //Fan-in
-                .flatMap(g -> {
-                    String type = g.key();
-                    Actor actor = actors.stream()
-                            .filter(actor1 -> actor1.supports(type))
-                            .findFirst()
-                            .orElseThrow(() -> new IllegalStateException("No actor type " + type));
-                    System.out.println("Agrupación por tipo");
-
-                        return g.sort((a,b) -> Integer.compare(b.getPriority(), a.getPriority()))
-                                .doOnNext(petition -> System.out.println(String.format("Petición [LEND] con ID: %s en cola",
-                                        petition.getPetitionId())))
-                                .concatMap( petition -> actor.handle(petition)
-                                        .doOnSubscribe(s -> System.out.println("Procesando petición con ID "+petition.getPetitionId())))
-                                .doOnNext(res -> System.out.println("Proceso exitoso"))
-                                .doOnError(err-> System.out.println("Procesamiento falló - "+err.getMessage()))
-                                .onErrorContinue((err, p) -> System.out.println("Petitición omitida " + err.getMessage()));
-
-                }).timeout(Duration.ofSeconds(5), Flux.just("Timeout exceeded")) //Control
-                .doOnNext(s -> System.out.println("Next: "+s))
-                .onErrorResume(err-> Flux.just("Error - "+ err.getMessage()))
-                .doOnComplete(() -> System.out.println("Orchestration complete"));
-    }
 }
